a711e7974a2b77ec33b72f48f98fdcc48cbd893e
Jeremy <jeremydiazp@gmail.com>
2023-05-24 17:36:37 +0700

    LoginLayout Done

--- app/src/main/java/com/example/interstatenow/MainActivity.kt
+++ app/src/main/java/com/example/interstatenow/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.hide()
+
         bottomNavigationView = findViewById(R.id.bottomNavigation)
         bottomNavigationView.setSelectedItemId(R.id.bottom_home)
 
