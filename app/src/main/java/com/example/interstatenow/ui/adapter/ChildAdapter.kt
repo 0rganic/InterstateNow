08a02904919ed1d0636cecebd45c05e9a40195b0
0rganic <jeremydiazp@gmail.com>
2023-06-04 19:24:28 +0700

    adapter + data layout#1

---
+++ app/src/main/java/com/example/interstatenow/ui/adapter/ChildAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.interstatenow.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ChildAdapter(private val listUser: List< >, private val onItemClick: (login: String) -> Unit): RecyclerView.Adapter<ChildAdapter.ViewHolder>()  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChildAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ChildAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/interstatenow/ui/adapter/ParentAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.interstatenow.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ParentAdapter (private val listUser: List<>, private val onItemClick: (login: String) -> Unit): RecyclerView.Adapter<ParentAdapter.ViewHolder>()  {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ParentAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ParentAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
